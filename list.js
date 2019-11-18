b75f3fb7d616e9375d67e361d95a10e3d381dd08
Sione Tenifa <johntenifa@yahoo.com>
2019-11-18 08:57:37 -0700

    delete todo'

--- list.js
+++ list.js
@@ -7,11 +7,19 @@ var input = prompt("What do you want to do?")
 while(input !== "quit"){
   //handle input
     if(input === 'list'){
-      console.log(todos)
+      todos.forEach(function(todo, i){
+        console.log(i + ": " + todo)
+      });
     } else if(input === "new"){
       var newTodo = prompt("Enter new todo");
       todos.push(newTodo)
       console.log(todos)
+    } else if (input === "delete"){
+      //ask for index of todo to be deleted
+      var index = prompt("Enter index of todo to delete");
+      //delete that todo
+      //splice
+      todos.splice(index, 1)
     }
     
   // ask for new input
