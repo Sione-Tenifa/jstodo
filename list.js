2164592fb595eb1ee024d434e5000d677f5899b3
Sione Tenifa <johntenifa@yahoo.com>
2019-11-18 09:07:02 -0700

    cleaned up code

--- list.js
+++ list.js
@@ -7,23 +7,38 @@ var input = prompt("What do you want to do?")
 while(input !== "quit"){
   //handle input
     if(input === 'list'){
-      todos.forEach(function(todo, i){
-        console.log(i + ": " + todo)
-      });
+        listTodos();
     } else if(input === "new"){
-      var newTodo = prompt("Enter new todo");
-      todos.push(newTodo)
-      console.log(todos)
+      addTodos();
     } else if (input === "delete"){
-      //ask for index of todo to be deleted
-      var index = prompt("Enter index of todo to delete");
-      //delete that todo
-      //splice
-      todos.splice(index, 1)
+      deleteTodos()
     }
-    
-  // ask for new input
+   // ask for new input
   input = prompt("What do you want to do?")
+}
+
+function listTodos(){
+  console.log("------")
+  todos.forEach(function(todo, i){
+    console.log(i + ": " + todo)
+  })
+  console.log("------")
+}
+
+function addTodos(){
+  var newTodo = prompt("Enter new todo");
+  todos.push(newTodo)
+  console.log(todos)
+  console.log('Added todo')
+}
+
+function deleteTodos(){
+  //ask for index of todo to be deleted
+  var index = prompt("Enter index of todo to delete");
+  //delete that todo
+  //splice
+  todos.splice(index, 1)
+  console.log("Deleted Todo")
 }
 
 console.log("ok you quit the app")
